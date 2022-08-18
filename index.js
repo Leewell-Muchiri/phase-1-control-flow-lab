f2667ce7473c311e4f64a9d0456b04abd8fbbfcb
leewellmacharia10@gmail.com <leewellmacharia10@gmail.com>
2022-08-18 15:47:35 +0300

    Copleted assignment

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 function scuberGreetingForFeet(){
-  // Write your code here!
-}
-
+  function scuberGreetingForFeet(feet){
+    if (feet<=400) {
+      return 'This one is on me!';
+    }
+    if (feet >2500) {
+      return 'No can do.';
+    } else if (feet >=2000) {
+      return 'I will gladly take your thirty bucks.';
+    }
+  }
 function ternaryCheckCity(){
-  // Write your code here!
+  function ternaryCheckCity(city){
+    return city === 'NYC'?'Ok, sounds good.' :'No go.'
+  }
 }
-
 function switchOnCharmFromTip(){
-  // Write your code here!
+  function switchOnCharmFromTip(tip){
+    switch(tip){
+      case 'generous':
+        return 'Thank yo so much.'
+      case 'not so geenrous':
+        return 'Thank you.'
+      default:
+        return 'Bye'
+    }
+  }
 }
